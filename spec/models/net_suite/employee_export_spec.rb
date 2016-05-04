9b3c0d4884c0d81ec2a5dc1fd230a04f9f3cc5bc
Rafael George <george.rafael@gmail.com>
2016-05-04 11:20:28 -0400

    Add exception handling to update

--- spec/models/net_suite/employee_export_spec.rb
+++ spec/models/net_suite/employee_export_spec.rb
@@ -40,4 +40,32 @@ RSpec.describe NetSuite::EmployeeExport do
       end
     end
   end
+
+  describe '#update' do
+    let(:profile) { double("Namely::Model", update: true, id: 'bunk') }
+
+    before do
+      allow(netsuite_client).to receive(:update_employee).and_raise(exception)
+    end
+
+    context 'when the request 400s because of a bad request' do
+      let(:response_body) { { providerMessage: "Bad request: you goofed" }.to_json }
+
+      it 'does not track the exception in raygun' do
+        expect(Raygun).to_not receive(:track_exception)
+
+        exporter.update
+      end
+    end
+
+    context 'when the request 400s because of cloud elements' do
+      let(:response_body) { { message: "java.lang.IllegalStateException: Session invalidation is in progress with different thread" }.to_json }
+
+      it 'does not track the exception in raygun' do
+        expect(Raygun).to receive(:track_exception)
+
+        exporter.update
+      end
+    end
+  end
 end
